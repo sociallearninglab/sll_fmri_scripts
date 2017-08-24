25ee2a71e0bdc1eb8717df86a3954a1ee0b8e2ba
Sajjad Torabian <sajjad@sh-5-36.local>
2017-08-24 01:33:22 +0000

    testing through sajjad

--- sll_fieldmap.sh
+++ sll_fieldmap.sh
@@ -5,10 +5,12 @@ SUBJ_DIR=$STUDY_DIR/$2
 FMAP_DIR=$SUBJ_DIR/fieldmap
 BOLD_DIR=$SUBJ_DIR/bold
 
-# Get fieldmap sequences
+# Get fieldmap sequence
 FMAP_SEQ=`printf %03d $3`
 FMAP_IN=$FMAP_DIR/$FMAP_SEQ/fieldmap-$FMAP_SEQ.nii
-echo $FMAP_IN
+echo 'Fieldmap image: ' $FMAP_IN
 
-# Get BOLD sequences
-echo $4
+# Get BOLD sequence
+BOLD_SEQ=`printf %03d $4`
+BOLD_IN=$BOLD_DIR/$BOLD_SEQ/f0-$BOLD_SEQ.nii
+echo 'BOLD image: ' $BOLD_IN
